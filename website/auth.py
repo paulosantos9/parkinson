cb3a583bbbca3b2eb9d075e195ac614bf0946afb
Paulo Santos <1170755@isep.ipp.pt>
2022-04-14 13:26:38 +0100

    Not letting users go to login and signup page when there is a current user

--- website/auth.py
+++ website/auth.py
@@ -47,47 +47,50 @@ def logout():
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    error = request.args.get('error', '')
-    typeOfContainer = request.args.get('typeOfContainer', '1')
-
-    if request.method == 'GET':
-        return render_template('index.html', error=error, typeOfContainer=int(typeOfContainer))
-    elif request.method == 'POST':
-        username = request.form.get('username')
-        email = request.form.get('email')
-        password = request.form.get('password')
-        password_confirm = request.form.get('password_confirm')
-
-        emailAlreadyUsed = True if User.query.filter_by(email=email).first() else False
-        usernameAlreadyUsed = True if User.query.filter_by(username=username).first() else False
-
-        error = None
-        typeOfContainer = 1 # 1 - register, 0 - login
-        if emailAlreadyUsed:
-            error = 'Este email já se encontra em utilização.'
-        elif usernameAlreadyUsed:
-            error = 'Este nome de utilizador já se encontra em utilização.'
-        elif len(email) < 6:
-            #flash('Email deve ter mais de 6 caracteres.', category='Erro')
-            error = 'Email deve ter mais de 6 caracteres.'
-        elif len(username) < 4:
-            #flash('Nome de utilizador deve ter mais de 3 caracteres.', category='erro')
-            error = 'Nome de utilizador deve ter mais de 3 caracteres.'
-        elif password != password_confirm:
-            #flash('Palavras-passe têm de ser iguais.', category='Erro')
-            error = 'Palavras-passe têm de ser iguais.'
-        elif len(password) < 7:
-            #flash('Palavra-passe deve ter mais de 6 caracteres.', category='Erro')
-            error = 'Palavra-passe deve ter mais de 6 caracteres.'
-        else:
-            # add user to database
-            #flash('Conta criada.', category='success')
-            new_user = User(email=email, username=username, password=generate_password_hash(password, method='sha256'))
-            db.session.add(new_user)
-            db.session.commit()
-            login_user(new_user, remember=True)
-            return redirect(url_for('views.main'))
-
-        return redirect(url_for('auth.signup', error=error, typeOfContainer=int(typeOfContainer)))
+    if current_user.is_authenticated:
+        return redirect(url_for('views.main'))
     else:
-        return redirect(url_for('auth.signup', error=error, typeOfContainer=int(typeOfContainer)))
+        error = request.args.get('error', '')
+        typeOfContainer = request.args.get('typeOfContainer', '1')
+    
+        if request.method == 'GET':
+            return render_template('index.html', error=error, typeOfContainer=int(typeOfContainer))
+        elif request.method == 'POST':
+            username = request.form.get('username')
+            email = request.form.get('email')
+            password = request.form.get('password')
+            password_confirm = request.form.get('password_confirm')
+
+            emailAlreadyUsed = True if User.query.filter_by(email=email).first() else False
+            usernameAlreadyUsed = True if User.query.filter_by(username=username).first() else False
+
+            error = None
+            typeOfContainer = 1 # 1 - register, 0 - login
+            if emailAlreadyUsed:
+                error = 'Este email já se encontra em utilização.'
+            elif usernameAlreadyUsed:
+                error = 'Este nome de utilizador já se encontra em utilização.'
+            elif len(email) < 6:
+                #flash('Email deve ter mais de 6 caracteres.', category='Erro')
+                error = 'Email deve ter mais de 6 caracteres.'
+            elif len(username) < 4:
+                #flash('Nome de utilizador deve ter mais de 3 caracteres.', category='erro')
+                error = 'Nome de utilizador deve ter mais de 3 caracteres.'
+            elif password != password_confirm:
+                #flash('Palavras-passe têm de ser iguais.', category='Erro')
+                error = 'Palavras-passe têm de ser iguais.'
+            elif len(password) < 7:
+                #flash('Palavra-passe deve ter mais de 6 caracteres.', category='Erro')
+                error = 'Palavra-passe deve ter mais de 6 caracteres.'
+            else:
+                # add user to database
+                #flash('Conta criada.', category='success')
+                new_user = User(email=email, username=username, password=generate_password_hash(password, method='sha256'))
+                db.session.add(new_user)
+                db.session.commit()
+                login_user(new_user, remember=True)
+                return redirect(url_for('views.main'))
+
+            return redirect(url_for('auth.signup', error=error, typeOfContainer=int(typeOfContainer)))
+        else:
+            return redirect(url_for('auth.signup', error=error, typeOfContainer=int(typeOfContainer)))
