634d19dd080689c8e45ce68269e2da398e81a81e
Paulo Santos <1170755@isep.ipp.pt>
2022-05-26 23:14:13 +0100

    adjust games list

--- website/static/js/games_list.js
+++ website/static/js/games_list.js
@@ -4,4 +4,5 @@ document.getElementById('back').onclick = function() {
 
 function changeDisplay(divId) {
   document.getElementById(divId).className === 'game-data-inner' ? document.getElementById(divId).classList.add("show-data") : document.getElementById(divId).classList.remove("show-data");
+  document.getElementById(divId+'-title').className === 'align-center' ? document.getElementById(divId+'-title').classList.add("hide-data") : document.getElementById(divId+'-title').classList.remove("hide-data");
 }
